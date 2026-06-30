7db923a1d0e0cca5c824ce2546307df3b41f07d7
Arshdeep Singh <136365795+Arsh-1313@users.noreply.github.com>
2026-06-30 12:39:39 +0530

    Time: 51 ms (13.75%) | Memory: 110.3 MB (5.05%) - LeetSync

--- 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-
         unordered_map<int, int> mp;
-
         for (int task : tasks) {
             mp[task]++;
         }
-
         // Min-heap based on frequency
-        priority_queue<pair<int, int>,
-                       vector<pair<int, int>>,
-                       function<bool(pair<int, int>, pair<int, int>)>>
-        pq([](pair<int, int> a, pair<int, int> b) {
-            return a.second > b.second;
-        });
-
+        priority_queue<
+    pair<int,int>,
+    vector<pair<int,int>>,
+    greater<pair<int,int>>
+> pq;
         for (auto &it : mp) {
             pq.push({it.first, it.second});
         }
-
         int count = 0;
-
         while (!pq.empty()) {
-
             auto top = pq.top();
             pq.pop();
-
             int task = top.first;
             int freq = top.second;
-
             if (freq == 0)
                 continue;
-
             else if (freq % 3 == 0) {
                 freq -= 3;
             }
-
             else if (freq == 1) {
                 return -1;
             }
-
             else {
                 freq -= 2;
             }
-
             count++;
-
             if (freq > 0)
                 pq.push({task, freq});
         }
-
         return count;
     }
 };
